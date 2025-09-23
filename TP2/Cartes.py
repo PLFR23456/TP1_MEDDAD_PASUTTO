a7fc765c3120427c022242984659b69c4e5abc6f
almed21 <ali.meddad@outlook.com>
2025-09-23 15:15:12 +0200

    Cartes

---
+++ TP2/Cartes.py
@@ -0,0 +1,37 @@
+
+from abc import ABC, abstractmethod
+import random
+
+cartes = {'Normale':1, 'Bonus':2, 'Malus':3, 'Chance':4}
+
+class Carte(ABC):
+    def __init__(self,cartes):
+        self.carte = cartes
+        self.point = None
+
+
+    @abstractmethod
+    def appliquer(self, joueur):
+        joueur.score += self.point
+
+
+
+
+class Carte_normale(Carte):
+    def __init__(self):
+        self.carte = cartes['Normale']
+        self.point = random.randrange(1,10)
+
+class Carte_bonus(Carte):
+    def __init__(self):
+        self.carte = cartes['Bonus']
+
+class Carte_malus(Carte):
+    def __init__(self):
+        self.carte = cartes['Malus']
+        self.point = -5
+
+class Carte_chance(Carte):
+    def __init__(self):
+        self.carte = cartes['Chance']
+        self.point = random.randrange(-5,15)
